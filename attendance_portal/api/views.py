645aa5fd3f6ef21ef8bb8171e139565485c6fa9b
Siddharth-gupta99 <siddharthgupta980681@gmail.com>
2019-10-16 14:57:50 +0530

    Modify settings.py

---
+++ attendance_portal/api/views.py
@@ -0,0 +1,7 @@
+# from django.shortcuts import render, get_object_or_404, redirect, ttpResponse
+# # from .models import Course, Enrollment, Lecture, Attendance
+# # Create your views here.
+
+# def api_func(request, lec_num):
+# 		lec_num
+# 		return HttpResponse("It is now")
