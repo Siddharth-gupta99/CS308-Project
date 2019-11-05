de7f28ad7b065c3bcfbe4a579571bda0a6d15c39
Shreyansh Kulshreshtha <shreyanshkuls@gmail.com>
2019-11-06 03:29:44 +0530

    Add currentTime API endpoint

--- attendance_portal/api/urls.py
+++ attendance_portal/api/urls.py
@@ -2,11 +2,8 @@ from rest_framework import  routers
 from . import views 
 from django.urls import path, include
 
-# router=routers.DefaultRouter()
-# router.register('l', views.ApiView)
-
 urlpatterns = [
-	# path('', include(router.urls))
 	path('lecture/<int:classroom_id>/', views.ApiView),
-	path('lecture/s', views.ApiView2)
+	path('lecture/s', views.ApiView2),
+	path('lecture/current-time', views.currentTimeView)
 	]
